1a4b16e466996a9ae99187d13cd86b499b40d143
anjy7 <anjyofficial@gmail.com>
2024-06-27 17:18:00 +0530

    fix landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,41 +1,18 @@
-import { Button, Card } from "@palanikannan1437/rc4-ui";
-
-const CARD_CONTENT = [
-  {
-    title: "Caching Tasks",
-    href: "https://turbo.build/repo/docs/core-concepts/caching",
-    cta: "Read More",
-  },
-  {
-    title: "Running Tasks",
-    href: "https://turbo.build/repo/docs/core-concepts/monorepos/running-tasks",
-    cta: "Read More",
-  },
-  {
-    title: "Configuration Options",
-    href: "https://turbo.build/repo/docs/reference/configuration",
-    cta: "Read More",
-  },
-];
-
 export default function Home() {
   return (
     <>
-      <div className="flex min-h-screen flex-col items-center justify-center py-2">
-        <main className="mx-auto w-auto px-4 pt-8 pb-8 sm:pt-8 lg:px-8">
-          <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
-            <span className="block bg-gradient-to-r from-brandred to-brandblue bg-clip-text px-2 text-transparent">
-              RC4Community
-            </span>
-            Demo
-          </h1>
-          <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
-            <Button />
-          </div>
-          <div className="mt-12 grid grid-cols-1 place-content-evenly gap-4 sm:grid-cols-3">
-            {CARD_CONTENT.map((card) => (
-              <Card key={card.title} {...card} />
-            ))}
+      <div className='flex min-h-screen flex-col items-center justify-center py-2 text-black'>
+        <main className='mx-auto w-auto px-4 pb-8 pt-8 sm:pt-8 lg:px-8'>
+          <div className='container mx-auto flex flex-col items-center gap-3 px-4 md:gap-5'>
+            <div className='mt-2 flex flex-col items-center gap-2 py-5'>
+              <h1 className='text-center text-4xl font-bold text-[#030c1a] md:text-5xl'>
+                Welcome to our <span className='text-[#f5455c]'>community</span>
+              </h1>
+              <p className='w-10/12 text-center text-[clamp(16px,4.5vw,20px)] font-normal text-[#87898d] md:w-8/12'>
+                Let's dream, share, and collaborate in shaping the future of the Rocket.Chat
+                ecosystem together
+              </p>
+            </div>
           </div>
         </main>
       </div>
